206eb888a054483f10e565e5390cdd686afaf6cf
Jonathan Long <jonathan_long@apple.com>
2016-10-20 22:35:27 -0700

    Begun adding database execution work.

---
+++ JLData.py
@@ -0,0 +1,26 @@
+from JloData import JloData
+
+'''
+JLData is a database representation for a Blog. 
+A Blog is defined as:
+Zone
+	id
+	Posts
+	Name
+
+Post
+	id
+	name
+	title
+	images?
+	comments?
+
+
+'''
+class JLData(JloData):
+	"""docstring for JLData"""
+	DB_NAME = "JLBLoggingData.db"
+
+	
+
+		
--- JloData.py
+++ JloData.py
@@ -1,40 +1,42 @@
 import json
-import os
+import sqlite3, os
 
 TABLES_KEY = "tables"
 PROPERTIES_KEY = "properties"
 TABLE_NAME_KEY = "table_name"
 FOREIGN_KEY_INFO_KEY = "keys"
 
+DEBUG_MODE = False
+def print_debug(s):
+	if (DEBUG_MODE): print create_table_command
+
 class JloData(object):
+	tables = [] # array of tupes such that tuple[0] is table_name and tuple[1] is an array of properties
 	def __init__(self, json_file=''):
 		assert(json_file != '')
 		data = None
-		# try:
 		with open(os.path.expanduser(json_file)) as data_file:
 			json_data = json.load(data_file)
-		# except IOError:
-		# 	print "Can not open file named: - " + str(json_file)
-		# 	exit(1)
-		# except:
-		# 	print "There was an unexpected error creating the DB"
-		# 	exit(1)
 		self.create_data_from_json(json_data)
 
 
 	def create_data_from_json(self, json_data):
 		tables = json_data[TABLES_KEY]
-		table_names = []
-		properties = [] # An array of tuples such that tuple[0] = property_name and tuple[1] = qualifier
-		# ex [('photoDestination' , 'TEXT NOT NULL'), ('photoID' , 'INTEGER NOT NULL')
-		foreign_key_info = [] # An array of arrays describing the foreign key values for each table
+
+		#Connect to the DB and get the cursor
+		db = sqlite3.connect(self.DB_NAME)
+		cursor = db.cursor()
 
 		for table in tables:
-			# table_names.append(tables[TABLE_NAME_KEY])
-			properties = zip(table[PROPERTIES_KEY].keys(), table[PROPERTIES_KEY].values())
-			# foreign_key_info = tables[FOREIGN_KEY_INFO_KEY]
-			create_table_command = JloData.create_table_command(table[TABLE_NAME_KEY], properties, table[FOREIGN_KEY_INFO_KEY])
-			print create_table_command
+			property_names = table[PROPERTIES_KEY].keys()
+			properties = zip(property_names, table[PROPERTIES_KEY].values())
+			table_name = table[TABLE_NAME_KEY] 
+
+			create_table_command = JloData.create_table_command(table_name, properties, table[FOREIGN_KEY_INFO_KEY])
+			print_debug(str(create_table_command))
+			cursor.execute(create_table_command)
+			
+
 
 
 	@staticmethod
